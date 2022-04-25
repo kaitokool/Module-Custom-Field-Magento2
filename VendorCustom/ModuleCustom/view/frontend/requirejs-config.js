320147feab4a370572ec8bcf0b443643aa109622
kaitokool <37665867+kaitokool@users.noreply.github.com>
2022-04-25 14:00:39 +0700

    Delete File

--- VendorCustom/ModuleCustom/view/frontend/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-     "map": {
-          "*": {
-               'Magento_Checkout/js/model/shipping-save-processor/default': 'VendorCustom_ModuleCustom/js/model/shipping-save-processor/default'
-          }
-     }
-};
